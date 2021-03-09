5e8076a8d549911b8ff706c00c3d004ccdee4134
zidane-sc <zidankren02@gmail.com>
2021-03-09 11:12:39 +0700

    add asset menu

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Http\Request;
 // Route::get('product/all', 'ApiProduct@all');
 
 // Route::post('trending', 'JsonController@trendingJson');
-Route::post('consumption', 'ConsumptionController@consumptionJson');
+Route::get('consumption', 'ConsumptionController@consumptionJson');
 
 Route::get('users', 'UsersApi@index');
 
