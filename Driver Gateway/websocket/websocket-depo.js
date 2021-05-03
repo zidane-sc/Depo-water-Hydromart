d42d960a0b31c12f1aea7ba922f707325f649da6
Ardhi Ramadhan <ardhiramadhan98@gmail.com>
2021-05-03 13:48:30 +0700

    New Logger

--- Driver Gateway/websocket/websocket-depo.js
+++ Driver Gateway/websocket/websocket-depo.js
@@ -11,7 +11,7 @@ const schedule = require('node-schedule');
 const axios = require('axios').default;
 
 let allValues = []
-const job = schedule.scheduleJob('* */5 * * * *', function () {
+const job = schedule.scheduleJob('*/5 * * * *', function () {
     console.log(allValues.length)
     console.log(allValues)
     if(allValues.length > 0){
